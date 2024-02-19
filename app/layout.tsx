17f6aa85fe8fc83c61d43856deaab67d99a2d67d
Houssem64 <fazeputin@yandex.com>
2024-02-19 05:46:11 +0100

    added Vercel SpeedInsights Package

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
-import Intro from "@/components/intro";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
   title: "Houssem Mehouachi",
   description:
