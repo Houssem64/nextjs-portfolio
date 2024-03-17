e4475e805fa1eafd1a8fb999202b8cafd5ed2c10
Houssem Mehouachi <fazeputin@yandex.com>
2024-03-18 00:19:18 +0100

    updated about me section

--- components/about.tsx
+++ components/about.tsx
@@ -11,19 +11,15 @@ export default function About() {
       transition={{ delay: 0.175 }}
       id="about"
     >
-      
+
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3"></p>
       <motion.p
         whileHover={{ scale: 1.2 }} // Zooms the text to 1.2x when hovered
         style={{ cursor: "pointer" }} // Changes cursor to pointer on hover>
       >
-        As a web developer, I excel in soft skills like communication,
-        problem-solving, and autonomy. My expertise lies in HTML, CSS,
-        JavaScript, Next.js, and React, complemented by hard skills including
-        Bash Scripting, Oracle databases, and hardware troubleshooting. I hold a
-        BTS in Web Development 
-        
+        I&apos;m a Web development enthusiast with a knack for turning ideas into visually stunning and interactive websites.  I love the challenge of crafting clean code and enjoy collaborating to solve complex problems.
+
       </motion.p>
     </motion.section>
   );
